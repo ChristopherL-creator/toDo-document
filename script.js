1ae6393868e3965bc6d0d8b566c0bd0f70285925
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-06 08:48:45 +0200

    3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const todo1 = new ToDo('Uscire', ToDo.PRIORITY.Medium, ['divertimento']) 
+const todo1 = new ToDo('Uscire', ToDo.PRIORITY.Medium, ['tag1']) 
 todo1.creationDate = new Date(2022, 6, 22);
 console.log(todo1.toString());  
 
@@ -7,14 +7,14 @@ const todo2 = new ToDo('heheh')
 todo2.creationDate = new Date(2022, 6, 22);
 console.log(todo2.toString()); 
 
-const paperino = new DeadLineToDo('manda mail', new Date(2022, 4, 7), ToDo.PRIORITY.High, ['hehe', ' hohoh']); 
+const paperino = new DeadLineToDo('manda mail', new Date(2022, 4, 7), ToDo.PRIORITY.High, ['tag2', ' tag3']); 
 console.log(paperino.toString()); 
 
-const paperina = new DeadLineToDo('manda mememel', new Date(2022, 6, 9), ToDo.PRIORITY.Max, ['uh uh']); 
+const paperina = new DeadLineToDo('manda mememel', new Date(2022, 6, 9), ToDo.PRIORITY.Max, ['tag4']); 
 console.log(paperina.toString()); 
 
 //  metto null perché va rispettato ordine di proprietà; attingerà a valore default;
-const topolino = new DeadLineToDo('compra pane', null, ToDo.PRIORITY.Medium, ['casa']);
+const topolino = new DeadLineToDo('compra pane', null, ToDo.PRIORITY.Medium, ['tag5']);
 console.log(topolino.toString()); 
 
 const toDoList = [todo1, todo2, paperino, paperina];  
@@ -33,11 +33,11 @@ function displayToDo(arrayTask) {
         titoloContainer.appendChild(titolo);
         const divFlex = document.createElement('div'); 
         divFlex.className = "task-flex"; 
-        for (let i = 0; i < arrayTask.length; i++) {
-            const tags = arrayTask[i];
+        for (let j = 0; j < arrayTask.length; j++) {
+            const tags = arrayTask[j];
             const divTag = document.createElement('div'); 
             divTag.className = 'tags-items';
-            const tagText = document.createTextNode(tags.tags); 
+            const tagText = document.createTextNode(j); 
             divTag.appendChild(tagText);
             divFlex.appendChild(divTag);
         } 
